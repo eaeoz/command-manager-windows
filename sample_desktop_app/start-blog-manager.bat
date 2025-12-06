6e8e2d925ad499eb2d107c2f54693ef42cf6ff69
eaeoz <sedatergoz@gmail.com>
2025-12-06 11:32:35 +0300

    old sample desktop app removed

--- sample_desktop_app/start-blog-manager.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Starting Blog Articles Manager...
-echo.
-echo Opening browser at http://localhost:3000/blog-articles-standalone.html
-echo.
-echo Press Ctrl+C to stop the server
-echo.
-start http://localhost:3000/blog-articles-standalone.html
-npx serve -l 3000
